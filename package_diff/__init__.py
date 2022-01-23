4efc34c4ec052cd963c10cce9595b1d06b04ba89
khwiri <5530052+khwiri@users.noreply.github.com>
2022-01-23 11:43:20 -0500

    :rotating_light: Fix exec-used Linter Error in setup.py
    by moving package version information to setup.cfg and using
    attr directive.

--- package_diff/__init__.py
+++ package_diff/__init__.py
@@ -1,1 +1,3 @@
 from .command import cli
+
+__version__ = '2022.01.21.dev0'
--- setup.py
+++ setup.py
@@ -1,27 +1,10 @@
-from os import path
-from functools import partial
-
 from setuptools import setup
-from setuptools import find_packages
-
-
-version_info  = {}
-package_join_ = partial(path.join, path.abspath(path.dirname(__file__)), 'package_diff')
-with open(package_join_('__version__.py'), encoding='utf-8') as f:
-    exec(f.read(), version_info)
 
 
 setup(
     name="package-diff",
-    version=version_info['__version__'],
-    packages=find_packages(),
     install_requires=[
         'click>=8.0.3',
         'rich>=10.16.2',
     ],
-    entry_points={
-        'console_scripts': [
-            'package-diff=package_diff:cli'
-        ]
-    }
 )
